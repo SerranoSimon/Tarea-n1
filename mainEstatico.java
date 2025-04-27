24048d30632f6edd77b1db3618aad129630eddef
SerranoSimon <sserrano2024@inf.udec.cl>
2025-04-26 23:39:27 -0600

    Presentacion MainEstatico

--- mainEstatico.java
+++ mainEstatico.java
@@ -1,18 +1,19 @@
 
 public class mainEstatico {
     public static void main(String[] args) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
-        //caso exitoso
+        //CASO EXITOSO:
+
         System.out.println("Caso exitoso:");
         Moneda m=new Moneda1000();
         Expendedor exp1=new Expendedor(1);
         for(int i=0;i<5;i++){
             Comprador c= new Comprador(m,i,exp1);
-            System.out.println(c.queConsumiste());
-            System.out.println(c.cuantoVuelto());
-
+            System.out.println("Compra "+(1+i)+":"+" consumiste "+c.queConsumiste()+", y tu vuelto es "+c.cuantoVuelto());
         }
-        //caso no queda de un producto en particular
-        System.out.println("Caso no queda de un producto en particular: ");
+
+        //CASO PRODUCTO AGOTADO
+        System.out.println("");
+        System.out.println("Caso producto agotado: ");
         try{
             Moneda m1=new Moneda1000();
             Expendedor exp2=new Expendedor(1);
@@ -20,11 +21,12 @@ public class mainEstatico {
             Comprador c2=new Comprador(m1,0,exp2);
         }
         catch(NoHayProductoException e){
-            System.out.println("Error en la compra: " + e.getMessage());
+            System.out.println("Tipo de error: " + e.getMessage());
 
         }
 
-        //caso moneda nula
+        //CASO MONEDA NULA
+        System.out.println("");
         System.out.println("Caso moneda nula: ");
         try {
             Moneda m1=null;
@@ -33,16 +35,18 @@ public class mainEstatico {
             System.out.println(c.queConsumiste());
 
         } catch (PagoIncorrectoException e) {
-            System.out.println("Error en la compra: " + e.getMessage());
+            System.out.println("Tipo de error: " + e.getMessage());
         }
-        //caso dinero insuficiente
+        //CASO DINERO INSUFICIENTE
+        System.out.println("");
+        System.out.println("Caso pago insuficiente: ");
         try{
             Moneda m2=new Moneda100();
             Comprador c= new Comprador(m2,3,new Expendedor(1));
             System.out.println(c.queConsumiste());
         }
         catch (PagoInsuficienteException e){
-            System.out.println("Error en la compra: " + e.getMessage());
+            System.out.println("Tipo de error: " + e.getMessage());
 
         }
 
