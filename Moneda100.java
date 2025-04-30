580dd86e929567422ce9e83b859bbd4a17e1533d
SerranoSimon <sserrano2024@inf.udec.cl>
2025-04-29 22:51:15 -0600

    Escritura javadoc de las Monedas

--- Moneda100.java
+++ Moneda100.java
@@ -1,8 +1,18 @@
+/**
+ * Clase que hereda de Moneda y representa una moneda de $100
+ */
 public class Moneda100 extends Moneda {
+    /**
+     * El constructor llama al constructor de Moneda
+     */
     public Moneda100(){
         super();
     }
 
+    /**
+     *
+     * @return el valor de la moneda en pesos
+     */
     @Override
     public int getValor() {
         return 100;
--- Moneda1000.java
+++ Moneda1000.java
@@ -1,8 +1,17 @@
+/**
+ * Clase que hereda de Moneda y representa una moneda de $1000
+ */
 public class Moneda1000 extends Moneda{
+    /**
+     * El constructor llama al constructor de Moneda
+     */
     public Moneda1000(){
         super();
     }
-
+    /**
+     *
+     * @return el valor de la moneda en pesos
+     */
     @Override
     public int getValor() {
         return 1000;
