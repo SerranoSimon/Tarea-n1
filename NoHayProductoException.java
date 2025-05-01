6fa6f1619ee31187fea5a237d0ec8776b0fc3e04
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-01 18:57:22 -0400

    Escritura javadoc PagoIncorrectoExceptionn

--- NoHayProductoException.java
+++ NoHayProductoException.java
@@ -1,11 +1,11 @@
 /**
- * Exception que se lanza si se intenta comprar un producto que no existe o se ingresa un índice inválido
- * se devuelve la misma moneda
+ * Exception que se lanza si se intenta comprar un producto que no existe o se ingresa un índice inválido.
+ * se devuelve la misma moneda.
  */
 public class NoHayProductoException extends Exception {
     /**
-     *Crea una nueva exception con un mensaje personalizado
-     * @param message describe el motivo del error
+     *Crea una nueva exception con un mensaje personalizado.
+     * @param message describe el motivo del error.
      */
     public NoHayProductoException(String message) {
         super(message);
--- PagoIncorrectoException.java
+++ PagoIncorrectoException.java
@@ -1,4 +1,12 @@
+/**
+ * Exception que se lanza si la moneda es nula.
+ * no entrega ni producto ni vuelto.
+ */
 public class PagoIncorrectoException extends Exception {
+    /**
+     *Crea una nueva exception con mensaje personalizado.
+     * @param message describe el motivo del error.
+     */
     public PagoIncorrectoException(String message) {
         super(message);
     }
