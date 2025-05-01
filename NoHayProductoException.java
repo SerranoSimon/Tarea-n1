75c9a848d1c7778d8859b6198273007224671450
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-01 18:53:58 -0400

    Escritura javadoc NoHayProductoException

--- NoHayProductoException.java
+++ NoHayProductoException.java
@@ -1,4 +1,12 @@
+/**
+ * Exception que se lanza si se intenta comprar un producto que no existe o se ingresa un índice inválido
+ * se devuelve la misma moneda
+ */
 public class NoHayProductoException extends Exception {
+    /**
+     *Crea una nueva exception con un mensaje personalizado
+     * @param message describe el motivo del error
+     */
     public NoHayProductoException(String message) {
         super(message);
     }
