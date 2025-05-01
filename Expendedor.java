948ada6844b0b5bc8b94219bfba73e07e77e3d0b
SerranoSimon <sserrano2024@inf.udec.cl>
2025-05-01 13:57:02 -0600

    Cambio en la creacion de productos usando su clase madre(Bebida o Dulce)

--- Expendedor.java
+++ Expendedor.java
@@ -23,11 +23,11 @@ public class Expendedor {
         this.super8=new DepositoGenerico<>();
         this.MonVu=new DepositoGenerico<>();
         for(int i=0; i<numProductos;i++){
-            Cocacola b1=new Cocacola(100+i);
-            Sprite b2=new Sprite(200+i);
-            Fanta b3=new Fanta(300+i);
-            Snickers e1=new Snickers(400+i);
-            Super8 e2=new Super8(500+i);
+            Bebida b1=new Cocacola(100+i);
+            Bebida b2=new Sprite(200+i);
+            Bebida b3=new Fanta(300+i);
+            Dulce e1=new Snickers(400+i);
+            Dulce e2=new Super8(500+i);
             coca.add(b1);
             sprite.add(b2);
             fanta.add(b3);
