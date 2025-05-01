cc0f7b099002058bb9bad165d00dbc06c1e2fc37
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-01 19:41:24 -0400

    Escritura javadoc MainInteractivo

--- MainInteractivo.java
+++ MainInteractivo.java
@@ -1,38 +1,57 @@
 import java.util.Scanner;
+
+/**
+ * Clase que simula una máquina expendedora.
+ * Permite a la persona seleccionar productos e ingresar monedas, simulando compras hasta que el usuario decida salirse.
+ */
 public class MainInteractivo {
+
     public static void main(String[] args) {
-        Expendedor exp = new Expendedor(3); //El número indica cuánta cantidad hay de cada producto
+        Expendedor exp = new Expendedor(3);
         Scanner sc = new Scanner(System.in);
-        while (true) { //creacion de un bucle para que el expendedor se ejecute constantemente hasta que el usuario decida irse con -1
+        while (true) {
             try {
+
                 Productos[] prod = Productos.values();
+                /**
+                 * Muestra los productos con su número asociado y su precio.
+                 */
                 for (int i = 0; i < prod.length; i++) {
                     System.out.println(i + ":" + prod[i].name() + " $" + prod[i].getPrecioProducto()); //Muestra cada tipo de producto con su número asociado y su precio
                 }
                 System.out.println("Ingrese -1 para salir.");
                 System.out.println("Número del producto:");
 
-                int selection = sc.nextInt(); //permite el ingreso de un valor int en la consola
-
+                int selection = sc.nextInt();
+                /**
+                 * La persona puede salir del programa ingresando -1 como número del producto.
+                 */
                 if (selection == -1){
                     System.out.println("Saliste de expendedor");
-                    break; //rompe el ciclo para salir de expendedor (cerrar el programa)
+                    break;
                 }
                 System.out.println("Ingrese un valor de moneda:");
                 int x = sc.nextInt();
+
                 Moneda m = null;
-                if (x == 100) { //creacion de una nueva moneda dependiendo del caso (100,500,1000)
+                /**
+                 * Crea una moneda dependiendo del valor ingresado (100,500,1000)
+                 */
+                if (x == 100) {
                     m = new Moneda100();
                 } else if (x == 500) {
                     m = new Moneda500();
                 } else if (x == 1000) {
                     m = new Moneda1000();
                 }
-                Comprador c = new Comprador(m, selection, exp); //crea un nuevo comprador con los dos parámetos necesarios
+                /**
+                 * Crea un nuevo comprador para producir la compra del producto, su consumo y el vuelto.
+                 */
+                Comprador c = new Comprador(m, selection, exp);
                 System.out.println("Consumiste: " + c.queConsumiste());
                 System.out.println("Tu vuelto es: " + c.cuantoVuelto());
-            } catch (PagoIncorrectoException | PagoInsuficienteException | NoHayProductoException e) { //si hubiese algún tipo de error, sería atrapado por catch (por ejemplo, un producto agotado o una cantidad de dinero insuficiente)
-                System.out.println("Tipo de error:" + e.getMessage()); //muestra el tipo de exception
+            } catch (PagoIncorrectoException | PagoInsuficienteException | NoHayProductoException e) {
+                System.out.println("Tipo de error:" + e.getMessage());
             }
         }
         sc.close();
