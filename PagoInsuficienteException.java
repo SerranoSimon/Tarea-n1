c299e8af1039076ac9515ba44d12efa22163ab19
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-01 18:59:03 -0400

    Escritura javadoc PagoIncorrectoExceptionn

--- PagoInsuficienteException.java
+++ PagoInsuficienteException.java
@@ -1,4 +1,12 @@
+/**
+ * Exception que se lanza si el valor de la moneda ingresada es menor al precio del producto.
+ * se devuelve la misma moneda.
+ */
 public class PagoInsuficienteException extends Exception {
+    /**
+     *Crea una nueva exception con mensaje personalizado.
+     * @param message describe el motivo del error.
+     */
     public PagoInsuficienteException(String message) {
         super(message);
     }
