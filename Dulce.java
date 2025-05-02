723514e640446acd64a7aa0ad5679b6b7a852645
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-01 20:02:20 -0400

    Actualización escritura javadoc Dulce

--- Dulce.java
+++ Dulce.java
@@ -3,6 +3,10 @@
  * no agrega atributos ni metodos nuevos, es solo para clasificar.
  */
 public abstract class Dulce extends Producto {
+    /**
+     *Llama al constructor de Producto.
+     * @param serie le da la serie al constructor de Producto.
+     */
     public Dulce(int serie){
         super(serie);
     }
